bad242d55bd79da0435184cc263fef6df881f81c
JaswanthSudha <iamsudhajaswanth@gmail.com>
2025-12-14 10:20:19 +0530

    Update Docker CI workflow to include SHA tagging and add about page route in Flask app

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello from CI/CD Docker!"
+@app.route("/about")
+def about():
+    return "This is about page"
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
